a211ef5eb8e334b7df9ed0058df8363bddd6566f
hyemin33 <joliie@naver.com>
2023-12-31 18:02:37 +0900

    Main 컴포넌트 분리

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
-import Image from "next/image";
-import styles from "@/app/page.module.css";
-import Logo from "../../../../public/logo.jpg";
+
+import Main from "../_component/Main";
 
 export default function Login() {
   const router = useRouter();
@@ -11,20 +9,7 @@ export default function Login() {
 
   return (
     <>
-      <div className={styles.left}>
-        <Image src={Logo} alt="logo" />
-      </div>
-      <div className={styles.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요.</h2>
-        <Link href="/i/flow/signup" className={styles.signup}>
-          계정 만들기
-        </Link>
-        <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="/login" className={styles.login}>
-          로그인
-        </Link>
-      </div>
+      <Main />
     </>
   );
 }
