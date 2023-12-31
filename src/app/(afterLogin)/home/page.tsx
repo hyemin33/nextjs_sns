1abb701773a9bbf661db3541654a7d24f0eff0b5
hyemin33 <joliie@naver.com>
2023-12-31 21:52:07 +0900

    PostForm 만들기(타이핑 외우기)

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,12 +1,14 @@
 import style from "./home.module.css";
 import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
+import PostForm from "./_component/PostForm";
 
 export default function Home() {
   return (
     <main className={style.main}>
       <TabProvider>
         <Tab />
+        <PostForm />
       </TabProvider>
     </main>
   );
